3f9be784599639244c80012d73366d6d6f1bdd70
Sole02 <ljaeseok02@gmail.com>
2026-04-14 20:26:47 +0900

    feat: 초기 프로젝트 구조 및 빌드 설정

---
+++ src/main/java/com/example/maple_api_sol_project/MapleApiSolProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.maple_api_sol_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapleApiSolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapleApiSolProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maple_api_sol_project/MapleApiSolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maple_api_sol_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleApiSolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
